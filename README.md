3fce96f1517b8a4319fe34901c5992ecc54e7260
tikuta <tikuta@linux>
2021-06-13 20:06:26 +0900

    ts

---
+++ README.md
@@ -0,0 +1,13 @@
+# hoge
+
+## How to run
+
+```
+npm run serve
+```
+
+
+## references
+
+* https://pixijs.io/examples/#/demos-basic/container.js
+* https://pixijs.io/examples/#/graphics/simple.js
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
+import * as PIXI from 'pixi.js';
+
 console.log("hell");
+
+const app = new PIXI.Application({
+    width: 800,
+    height : 600,
+});
+
+document.body.appendChild(app.view);
+
+const container = new PIXI.Container();
+
+app.stage.addChild(container);
+
+const graphics = new PIXI.Graphics();
+
+container.addChild(graphics);
+
+
+container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
+
+
+container.pivot.x = container.width / 2;
+container.pivot.y =  container.height / 2;
+
+
+for (let x = -10; x <= 10; ++x){
+    for (let y = -20; y <= 20; ++y){
+        graphics.beginFill(0xDE3249, 1);
+        graphics.drawCircle(x * 10, y * 10, 3);
+        graphics.endFill();
+    }
+}
